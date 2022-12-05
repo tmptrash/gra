88da102d9cf9027a6eb4f56825fdea83730b9163
flatline <deadbrainman@gmail.com>
2022-12-05 18:44:51 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'green';
+ctx.fillRect(10, 10, 100, 200);
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+ const path = require('path');
+ const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: __dirname + '/dist/index.html',
+      inject: 'body'
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
