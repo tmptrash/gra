ea8c4f1289acbcf2eb346a4292a5331ac0c8fee3
flatline <deadbrainman@gmail.com>
2022-12-18 21:35:18 +0200

    fixed an issue with jumping and running

--- src/hero.js
+++ src/hero.js
@@ -42,16 +42,35 @@ function update(hero) {
   hero.vX = 0
 
   // walk left or right
-  if (hero.pressed.d) { sprite.img = sprite.imgs.walkRight, hero.vX = Config.moveSpeed, sprite.dir = RIGHT }
-  if (hero.pressed.a) { sprite.img = sprite.imgs.walkLeft, hero.vX = -Config.moveSpeed, sprite.dir = LEFT }
+  if (hero.pressed.d) {
+    !hero.isJumping && (sprite.img = sprite.imgs.walkRight)
+    hero.vX = Config.moveSpeed
+    sprite.dir = RIGHT
+  }
+  if (hero.pressed.a) {
+    !hero.isJumping && (sprite.img = sprite.imgs.walkLeft)
+    hero.vX = -Config.moveSpeed
+    sprite.dir = LEFT
+  }
 
   // idle
-  hero.vX === 0 && (sprite.img = (sprite.dir === RIGHT) ? sprite.imgs.idleRight : sprite.imgs.idleLeft)
+  hero.vX === 0 && !hero.isJumping && (sprite.img = (sprite.dir === RIGHT) ? sprite.imgs.idleRight : sprite.imgs.idleLeft)
 
-  // jump
+  // stop jumping if it's a ground
   if (sprite.y + hero.vY + sprite.img.height < Config.height) hero.vY += Config.gravity
   else { hero.vY = 0, sprite.y = Config.height - sprite.img.height, hero.isJumping = false }
-  hero.isJumping && (sprite.img = (sprite.dir === RIGHT) ? sprite.imgs.jumpRight : sprite.imgs.jumpLeft)
+  
+  // update jump frames depending on dir
+  if (hero.isJumping) {
+    const dirRight = sprite.img === sprite.imgs.jumpRight 
+    if (sprite.dir === RIGHT) {
+      sprite.img = sprite.imgs.jumpRight
+      !dirRight && (sprite.img.frames.frame = sprite.imgs.jumpLeft.frames.frame)
+    } else {
+      sprite.img = sprite.imgs.jumpLeft
+      dirRight && (sprite.img.frames.frame = sprite.imgs.jumpRight.frames.frame)
+    }
+  }
 }
 
 function onJumpKeyDown(hero) {
