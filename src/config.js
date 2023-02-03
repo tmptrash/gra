de73270c503f237f23caa65de04d12cf41eccfeb
flatline <deadbrainman@gmail.com>
2023-02-03 17:34:34 +0200

    - Removed debug mashroom

--- src/config.js
+++ src/config.js
@@ -347,7 +347,6 @@ export default Config = {
       0: [
         //[[{x: 100, y: 200}, {idle: [GunAnimPath,   9,  150]}], 'gun',     'foundGun',     (i, pick) => {Shared.hero.gun = true, pick(i)}],
         //[[{x: 200, y: 300}, {idle: [BulletsPath,   9,  150]}], 'bullets', 'foundBullets', (i, pick) => {Shared.hero.bullets += Config.bulletsAmount, pick(i, false)}]
-        [[{x: 100, y: 128}, {idle: [Mashroom9Path, 9,  100]}], 'mashroom','foundMashroom',onMashroom]
       ],
       2: [
         [[{x: 128, y: 700}, {idle: [HeartAnimPath, 9,  100]}], 'heart',   'foundHeart',   (i, pick) => {Shared.hero.life++, pick(i, false)}]
