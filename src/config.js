5b271004693f287092e9f8e14841c2c45c872248
flatline <deadbrainman@gmail.com>
2023-02-23 13:23:50 +0200

    - Removed debug info

--- src/config.js
+++ src/config.js
@@ -62,7 +62,7 @@ export default Config = {
   // game related
   useSetTimeout: false,
   setTimeoutDelay: 7,
-  debug: true,
+  debug: false,
   debugPos: [300, 40],
   fpsPos: [WIDTH / 2 - 100, 20],
   logoPos: [256, 50],
