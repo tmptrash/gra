e70a34c21b142f81ff4a7bab4b33003af60d1415
flatline <deadbrainman@gmail.com>
2023-02-28 13:36:43 +0200

    - Added more realistic firefly movement (#36)

--- src/firefly.js
+++ src/firefly.js
@@ -4,16 +4,26 @@ import { Sprite, draw as drawSprite, update as updateSprite } from './sprite'
 import { xyBarrier } from './barriers'
 import { on } from './utils'
 
-const SPEED = 3
-const HALF_SPEED = SPEED / 2
-const MAX_AMOUNT = 60
-const DELAY = 40
+const MAX_AMOUNT = 50
+const DELAY = 20
+const SPEED = .8
+const DIRS = new Int8Array([
+   0,  1,
+   1,  1,
+   1,  0,
+   1, -1,
+   0, -1,
+  -1, -1,
+  -1,  0,
+  -1,  1
+])
 
 export function Firefly() {
   const ff = {
-    sprites: [],
-    hidden: [],
-    t: []
+    sprites: Array(MAX_AMOUNT),
+    hidden: Array(MAX_AMOUNT),
+    dirs: new Float32Array(MAX_AMOUNT * 2),
+    t: new Float64Array(MAX_AMOUNT)
   }
   const w = Config.width - 64
   const h = Config.height - 64
@@ -21,10 +31,11 @@ export function Firefly() {
   for (let i = 0; i < MAX_AMOUNT; i++) {
     cfg[0].x = Math.random() * w + 32
     cfg[0].y = Math.random() * h + 32
-    ff.sprites.push(Sprite(...cfg))
-    ff.hidden.push(false)
-    ff.t.push(0)
+    ff.sprites[i] = Sprite(...cfg)
+    ff.hidden[i] = false
+    ff.t[i] = 0
     ff.hidden[i] = xyBarrier(cfg[0].x, cfg[0].y)
+    ff.dirs[i] = Math.floor(Math.random() * 8)
   }
   on(Shared.obs, 'change-room', onChangeRoom.bind(null, ff))
 
@@ -44,13 +55,12 @@ export function update(f) {
   const t = performance.now()
   f.sprites.forEach((s, i) => {
     if (t - f.t[i] > DELAY) {
-      let x = s.x + Math.random() * SPEED - HALF_SPEED
-      let y = s.y + Math.random() * SPEED - HALF_SPEED
+      let [d, x, y] = newXY(f.dirs[i], s.x, s.y)
       if (x < 32) x = 32
       else if (x > w) x = w
       if (y < 32) y = 32
       else if (y > h) y = h
-      if (!xyBarrier(x, y)) s.x = x, s.y = y
+      if (!xyBarrier(x, y)) s.x = x, s.y = y, f.dirs[i] = d
       updateSprite(s)
       f.t[i] = t
     }
@@ -65,4 +75,12 @@ function onChangeRoom(f) {
     s.y = Math.random() * h + 32
     f.hidden[i] = xyBarrier(s.x, s.y)
   })
+}
+
+function newXY(d, x, y) {
+  const dir = Math.floor(Math.random() * 3) - 1
+  d += dir
+  if (d < 0) d = 7
+  else if (d > 7) d = 0
+  return [d, x + DIRS[d * 2] * SPEED, y + DIRS[d * 2 + 1] * SPEED]
 }
