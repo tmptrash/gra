f415eb73ead90f2d995e01d5837a8abf3c10e8b8
flatline <deadbrainman@gmail.com>
2023-03-09 23:48:16 +0200

    - Fix z-order issue with a lake

--- src/config.js
+++ src/config.js
@@ -601,16 +601,16 @@ export default Config = {
         ['Water',  {params:  [415, 712, 1023, 712, 6, 27], drop: [{x: 0, y:0}, {idle: [A.DropDownPath, 12, 40]}], pos: 5 }]
       ],
       38: [
-        ['Drop',   {sprite1: [{x: 400, y: 576}, A.DropPath], sprite2: [{x: 400, y: 576}, {idle: [A.DropDownPath, 12, 80]}], pos: 1, before: 26, sound: 'drop1', speed:  8, delay: 2000}],
-        ['Drop',   {sprite1: [{x: 627, y: 576}, A.DropPath], sprite2: [{x: 627, y: 576}, {idle: [A.DropDownPath, 12, 80]}], pos: 1, before: 26, sound: 'drop3', speed:  8, delay: 1000}],
-        ['Drop',   {sprite1: [{x: 800, y: 576}, A.DropPath], sprite2: [{x: 800, y: 576}, {idle: [A.DropDownPath, 12, 80]}], pos: 1, before: 26, sound: 'drop2', speed:  8, delay: 3000}],
-        ['Water',  {params:  [0, 712, 1023, 712, 6, 27], drop: [{x: 0, y:0}, {idle: [A.DropDownPath, 12, 40]}], pos: 11 }]
+        ['Drop',   {sprite1: [{x: 400, y: 576}, A.DropPath], sprite2: [{x: 400, y: 576}, {idle: [A.DropDownPath, 12, 80]}], pos: 1, before: 26, sound: 'drop1', speed: 8, delay: 2000}],
+        ['Drop',   {sprite1: [{x: 627, y: 576}, A.DropPath], sprite2: [{x: 627, y: 576}, {idle: [A.DropDownPath, 12, 80]}], pos: 1, before: 26, sound: 'drop3', speed: 8, delay: 1000}],
+        ['Drop',   {sprite1: [{x: 800, y: 576}, A.DropPath], sprite2: [{x: 800, y: 576}, {idle: [A.DropDownPath, 12, 80]}], pos: 1, before: 26, sound: 'drop2', speed: 8, delay: 3000}],
+        ['Water',  {params:  [0, 712, 1023, 712, 6, 27], drop: [{x: 0, y:0}, {idle: [A.DropDownPath, 12, 40]}], pos: 8 }]
       ],
       39: [
         ['Drop',   {sprite1: [{x: 544, y: 544}, A.DropPath], sprite2: [{x: 544, y: 544}, {idle: [A.DropDownPath, 12, 80]}], pos: 1, before: 23, sound: 'drop1', speed:  8, delay: 2000}],
         ['Drop',   {sprite1: [{x: 769, y: 447}, A.DropPath], sprite2: [{x: 769, y: 447}, {idle: [A.DropDownPath, 12, 80]}], pos: 1, sound: 'drop3', speed:  8, delay: 1000}],
         ['Drop',   {sprite1: [{x: 621, y: 319}, A.DropPath], sprite2: [{x: 621, y: 319}, {idle: [A.DropDownPath, 12, 80]}], pos: 1, before: 23, sound: 'drop2', speed:  8, delay: 3000}],
-        ['Water',  {params:  [0, 715, 673, 715, 6, 24], drop: [{x: 0, y:0}, {idle: [A.DropDownPath, 12, 40]}], pos: 11 }]
+        ['Water',  {params:  [0, 715, 673, 715, 6, 24], drop: [{x: 0, y:0}, {idle: [A.DropDownPath, 12, 40]}], pos: 9 }]
       ]
     }
   },
