20a6d19a84d4f1c5ea913737286406c3423b91e3
flatline <deadbrainman@gmail.com>
2023-03-22 16:43:12 +0200

    - Fixed an issue with observer

--- src/effects.js
+++ src/effects.js
@@ -1,11 +1,16 @@
 import { Flashlight, draw as drawFl, update as updateFl } from './flashlight'
 import { Firefly, draw as drawFf, update as updateFf } from './firefly'
+import { ons } from './utils'
 
 export function Effects() {
-  return {
+  const effects = {
     fl: Flashlight(),
     ff: Firefly()
   }
+  ons(effects.fl.listeners)
+  ons(effects.ff.listeners)
+
+  return effects
 }
 
 export function draw(e) {
